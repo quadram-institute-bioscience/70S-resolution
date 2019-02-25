085af990030fe348199f363fdd845418729c3bb6
Leonardo de-Oliveira-Martins (QIB) <Leonardo Martins leomrtns@gmail.com>
2019-02-25 10:12:53 +0000

    paralog operon analysis with pseudomonas and staph (200 sequences)

--- 014_results/plot_tree.R
+++ 014_results/plot_tree.R
@@ -2,7 +2,7 @@
 library(ape);
 library(RColorBrewer);
 
-png ("operon%02d.png", width = 6000, height= 10000, bg = "white");
+png ("operon%02d.png", width = 6000, height= 12000, bg = "white");
 par (tcl=-0.15, mgp=c(2.1,0.2,0), mar=c(1,1,0.1,0.1));
 par (font=2, cex=2, col.main="gray10", cex.axis=1.5, cex.main=5, cex.lab=1.2);
 #layout(matrix(1:2, 1, 2, byrow = TRUE))
@@ -35,11 +35,12 @@ for (i in 1:length(treefile)) {
             if ( grepl(paste(minclad[j],"_",sep=""),tre$tip.label[k]) ) col_mic[k] <- mic[j];
         }
     }
-    my_cex = ifelse (len < 300, 4, 1.2);
+    my_cex = ifelse (len < 500, 1.2, .9);
     plot (tre, cex=my_cex, node.depth=1, font=2, edge.width=12, tip.color=col_mac, no.margin=T, label.offset=0.001, direction="rightwards");
     x = unlist(strsplit(treefile[i],'[.]treefile'))  # split is regexp, so dot is special
     title(main=x[1], line = -3);
-    legend("bottomleft", legend=majclad, bty="n", fill=mac, border=mac, cex=5, inset=0.1);
+    if ( i == 1) { legend("bottomleft", legend=majclad, bty="n", fill=mac, border=mac, cex=5, inset=0.1);
+    } else { legend("topright", legend=majclad, bty="n", fill=mac, border=mac, cex=5, inset=0.1); }
 }
 dev.off(); quit();
 
